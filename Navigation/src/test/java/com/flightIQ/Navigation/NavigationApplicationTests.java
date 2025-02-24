666267cd18ce8924cc58d9e3623960d5449181f1
Abel Chacko Mathew <achusep19@gmail.com>
2025-02-24 17:17:00 -0500

    Created and wrote skeleton code to test cloud sql connection

--- Navigation/src/test/java/com/flightIQ/Navigation/NavigationApplicationTests.java
+++ Navigation/src/test/java/com/flightIQ/Navigation/NavigationApplicationTests.java
@@ -1,13 +1,36 @@
 package com.flightIQ.Navigation;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+
+@EnabledIfSystemProperty(named = "it.cloudsql", matches = "true")
+@TestPropertySource(locations = "/Navigation/src/main/resources/application-secret.properties")
+@ExtendWith(SpringExtension.class)
+@SpringBootTest (
+		webEnvironment=WebEnvironment.RANDOM_PORT,
+		classes={NavigationApplication.class},
+		properties= {
+		  "spring.datasource.username=${spring.datasource.username}",
+		  "spring.datasource.password=${spring.datasource.password}",
+		  "spring.cloud.gcp.sql.database-name=flightiq",
+		  "spring.cloud.gcp.sql.instance-connection-name=${spring.cloud.gcp.sql.instance-connection-name}"
+		})
 class NavigationApplicationTests {
 
+	@Autowired 
+	private CommandLineRunner commandLineRunner;
+	
 	@Test
-	void contextLoads() {
+	void testGoogleCloudSQLConnection() {
+		
 	}
 
 }
