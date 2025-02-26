28b8f7f71bce7cadad28a2f47874e2caca6c8297
Abel Chacko Mathew <achusep19@gmail.com>
2025-02-26 00:41:58 -0500

    Added XML annotations to serialize route data for API responses

--- Navigation/src/main/java/com/flightIQ/Navigation/DTO/FlightRoute.java
+++ Navigation/src/main/java/com/flightIQ/Navigation/DTO/FlightRoute.java
@@ -1,5 +1,16 @@
 package com.flightIQ.Navigation.DTO;
 
-public class FlightRoute {
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
 
+// Used to send the route in an XML format back to the client
+@XmlRootElement(name = "flightRoute")
+public class FlightRoute {
+	private String departureICAOCode;
+	private String arrivalICAOCode;
+	
+	// List of waypoints
+	// Route with fixxes and origin and destination airport
+	
+	
 }
