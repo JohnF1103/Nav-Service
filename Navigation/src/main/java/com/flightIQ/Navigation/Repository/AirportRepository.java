6522b3fa81c1846d16fb67d28d916c7002b1814c
JohnF1103 <johnefoster@icloud.com>
2025-02-26 21:17:59 -0500

    DB  connection outline ***ERROR***

---
+++ Navigation/src/main/java/com/flightIQ/Navigation/Repository/AirportRepository.java
@@ -0,0 +1,9 @@
+package com.flightIQ.Navigation.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.flightIQ.Navigation.DTO.AirportEntity;
+
+public interface AirportRepository extends JpaRepository<AirportEntity, Integer>{
+
+}
