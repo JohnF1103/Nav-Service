209e010f2f097376bd751e550143511fe0c94ef1
Abel Chacko Mathew <achusep19@gmail.com>
2025-02-28 15:50:59 -0500

    Created skeleton for integration tests to validate Cloud SQL database connectivity

---
+++ Navigation/src/test/java/com/flightIQ/Navigation/CloudSQLConnectionTest.java
@@ -0,0 +1,30 @@
+package com.flightIQ.Navigation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+
+import com.flightIQ.Navigation.Repository.AirportRepository;
+
+@DataJpaTest
+@TestPropertySource(locations = "/Navigation/src/main/resources/application-secret.properties")
+@AutoConfigureTestDatabase(replace=AutoConfigureTestDatabase.Replace.NONE)
+class CloudSQLConnectionTest {
+	
+	@Autowired
+	private final AirportRepository airportRepository;
+
+	@Test
+	void testAirportDBConnection() {
+		
+	}
+
+	@Test
+	void testFIXXDBConnection() {
+		
+	}
+}
