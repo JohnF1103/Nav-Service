49a62aeb0f4642acb5adb2f863bbb1d83f7a7939
russellchao <russchaoman@gmail.com>
2025-03-14 17:10:47 -0400

    Pulling from dev-abel

--- Navigation/src/main/java/com/flightIQ/Navigation/Repository/AirportRepository.java
+++ Navigation/src/main/java/com/flightIQ/Navigation/Repository/AirportRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.flightIQ.Navigation.DTO.AirportEntity;
 
 public interface AirportRepository extends JpaRepository<AirportEntity, Integer>{
-
+    
 }
