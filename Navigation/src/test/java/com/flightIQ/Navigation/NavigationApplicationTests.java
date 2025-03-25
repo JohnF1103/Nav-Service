dccf2a63f170d6c3a3c4925430041e90017a4693
JohnF1103 <johnefoster@icloud.com>
2025-03-25 18:11:03 -0400

    russel wind correction.

--- Navigation/src/test/java/com/flightIQ/Navigation/NavigationApplicationTests.java
+++ Navigation/src/test/java/com/flightIQ/Navigation/NavigationApplicationTests.java
@@ -1,13 +1,21 @@
 package com.flightIQ.Navigation;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import com.flightIQ.Navigation.Service.NavigationServiceImpl;
 
-@SpringBootTest
-class NavigationApplicationTests {
+public class NavigationApplicationTests {
+    public static void main(String[] args) {
+        NavigationServiceImpl navigationService = new NavigationServiceImpl();
 
-	@Test
-	void contextLoads() {
-	}
+        // Example call to ComputeTrueCourse
+        int plottedCourse = 90;
+        String windsAloft = "180@25";
+        double lat = 26.2473600;
+        double lon = -80.1111272;
+        int trueAirspeed = 150;
 
+        int trueCourse = navigationService.ComputeTrueCourse(plottedCourse, windsAloft, lat, lon, trueAirspeed);
+        System.out.println("Computed True Course: " + trueCourse);
+
+  
+    }
 }
