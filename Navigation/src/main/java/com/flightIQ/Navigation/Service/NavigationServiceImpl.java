a9fc32baad371743307415739aabdafea999a507
JohnF1103 <jfoster@screeningsolution.com>
2025-03-25 11:20:10 -0400

    russels skeleton.

--- Navigation/src/main/java/com/flightIQ/Navigation/Service/NavigationServiceImpl.java
+++ Navigation/src/main/java/com/flightIQ/Navigation/Service/NavigationServiceImpl.java
@@ -8,37 +8,36 @@ import org.springframework.stereotype.Service;
 @Service
 public class NavigationServiceImpl implements Navigation_svc {
 
+    public int ComputeTrueCourse(int plottedCourse, String WindsAloftAtCruise, double lat, double lon){
+
+
+
+        //adjust plotted course for wind correction
+        
+
+        //adjust for E/W variation
+
+        int declination = 0;
+
+        //adjust for compass error FOR STEER, maybe import table from 75200
+
+
+        if (lat >= 0) {
+            declination = (int) (10 - (lat / 10.0));
+        } else {
+            declination = (int) (-10 + (lat / 10.0));
+        }
+
+
+        return declination;
+    }
+
     @Override
     public String GetATISOFDestination(String X_coord, String Y_coord, String DestAirportCode) {
         // TODO Auto-generated method stub
 
-
-        String response = ""
-        
-        if distance formula (x,y) , DestAirportCode <= 10{
-
-
-            try{
-                //make GET REQ using https://www.geeksforgeeks.org/spring-resttemplate/
-
-                freqAPI_reponse = https://frq-svc-272565453292.us-central1.run.app/api/v1/getAirportFrequencies?airportCode=KPMP
-
-                //parse out ATIS 
-
-                String ATIS_response = //maybe global var in function. 
-
-                response = ATIS_response;
-                
-            }catch{
-
-                trow exception
-
-            }
-
-        }else{
-
-            response = ""
-        }
+        System.out.println(ComputeTrueCourse(0, DestAirportCode, 26.2473600,-80.1111272 ));
+        String response = "";
         return response;
     }
 
