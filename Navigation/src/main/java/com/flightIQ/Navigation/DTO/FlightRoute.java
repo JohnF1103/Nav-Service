e12b1a07f3a6b006bab1a43d5abdaf0202f92d49
Abel Chacko Mathew <achusep19@gmail.com>
2025-03-29 23:29:27 -0400

    Created a DTO to store final flight route

--- Navigation/src/main/java/com/flightIQ/Navigation/DTO/FlightRoute.java
+++ Navigation/src/main/java/com/flightIQ/Navigation/DTO/FlightRoute.java
@@ -1,16 +1,20 @@
 package com.flightIQ.Navigation.DTO;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlRootElement;
+import lombok.Getter;
+import lombok.Setter;
 
 // Used to send the route in an XML format back to the client
+@Getter
+@Setter
 @XmlRootElement(name = "flightRoute")
 public class FlightRoute {
+	private long routeId;
 	private String departureICAOCode;
 	private String arrivalICAOCode;
-	
-	// List of waypoints
-	// Route with fixxes and origin and destination airport
-	
-	
+	private List<String> route = new ArrayList<>();
 }
