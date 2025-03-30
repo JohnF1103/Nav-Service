373985c7af2471fc7a45fe7cc3fc5bae0477b6a4
Abel Chacko Mathew <achusep19@gmail.com>
2025-03-29 23:26:07 -0400

    Replaced JPA repository interfaces with Neo4j-compatible implementations

--- Navigation/src/main/java/com/flightIQ/Navigation/Repository/AirportRepository.java
+++ Navigation/src/main/java/com/flightIQ/Navigation/Repository/AirportRepository.java
@@ -2,13 +2,13 @@ package com.flightIQ.Navigation.Repository;
 
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.stereotype.Repository;
 
 import com.flightIQ.Navigation.Models.Airport;
 
 @Repository
-public interface AirportRepository extends JpaRepository<Airport, String> {
+public interface AirportRepository extends Neo4jRepository<Airport, String> {
 	Optional<Airport> findByIdent(String ident);
 	Optional<Airport> findByIcao(String icao);
 }
--- Navigation/src/main/java/com/flightIQ/Navigation/Repository/FIXRepository.java
+++ Navigation/src/main/java/com/flightIQ/Navigation/Repository/FIXRepository.java
@@ -1,11 +1,12 @@
 package com.flightIQ.Navigation.Repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.stereotype.Repository;
 
 import com.flightIQ.Navigation.Models.FIXX;
 
 @Repository
-public interface FIXRepository extends JpaRepository<FIXX, String> {
-	FIXX findByFixId(String fixId);
+public interface FIXRepository extends Neo4jRepository<FIXX, String> {
+	FIXX findByFixxId(String fixxId);
 }
