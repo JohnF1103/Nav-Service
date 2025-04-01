7027e2e0f1b4958cdf9745c44e10448ae4cac4e8
Abel Chacko Mathew <achusep19@gmail.com>
2025-04-01 17:59:50 -0400

    Minor change

--- Navigation/src/main/java/com/flightIQ/Navigation/Service/NavigationServiceImpl.java
+++ Navigation/src/main/java/com/flightIQ/Navigation/Service/NavigationServiceImpl.java
@@ -4,9 +4,7 @@ package com.flightIQ.Navigation.Service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-=======
 import java.util.ArrayList;
->>>>>>> integration
 import org.springframework.stereotype.Service;
 import com.flightIQ.Navigation.DTO.RouteNode;
 
