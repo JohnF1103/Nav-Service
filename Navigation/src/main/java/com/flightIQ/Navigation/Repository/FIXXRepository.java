19cfa752dd25af7b64da6f69ab29caa82572a8a8
Abel Chacko Mathew <achusep19@gmail.com>
2025-04-06 23:43:17 -0400

    Minor updates

--- Navigation/src/main/java/com/flightIQ/Navigation/Repository/FIXXRepository.java
+++ Navigation/src/main/java/com/flightIQ/Navigation/Repository/FIXXRepository.java
@@ -10,5 +10,5 @@ import com.flightIQ.Navigation.Models.FIXX;
 
 @Repository
 public interface FIXXRepository extends Neo4jRepository<FIXX, String> {
-	Optional<FIXX> findByFixxId(String fixxId);
+	FIXX findByFixxId(String fixxId);
 }
